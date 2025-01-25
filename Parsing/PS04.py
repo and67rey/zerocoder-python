4dc8f5154110aec6796de3055c199c6e7c5706be
Andrey <adulub@spinupper.com>
2025-01-25 13:21:11 -0500

    version 1.2

--- Parsing/PS04.py
+++ Parsing/PS04.py
@@ -50,37 +50,12 @@ enter_point = browser.find_element(By.LINK_TEXT, user_search)
 enter_point.click()
 
 # Выбор вариантов для продолжения
-case = input('Сделайте выбор:\n 0 - для завершения;\n 1 - листать параграфы на странице;\n 2 - перейти на одну из связанных страниц; \nВаш выбор: ')
-
-if case == '0':
-    print('Завершение программы')
-    time.sleep(3)
-elif case == '1':
-    paragraphs = browser.find_elements(By.TAG_NAME, "p")
-    print(f'Всего параграфов: {len(paragraphs)}')
-    for paragraph in paragraphs:
-        print(paragraph.text, '\n')
-        k = input('Нажмите Enter для вывода следующего параграфа или Q для завершения: ')
-        if k.lower() == 'q':
-            break
-elif case == '2':
-    all_links = find_all_links(enter_point, browser)
-    # Выбор подходящей ссылки для перехода
-    for title, link in all_links.items():
-        transit_link = link
-        print(f"Название ссылки: {title},\n ссылка: {link}\n")
-        choice = input(
-            'Нажмите Enter, чтобы дальше листать ссылки, 1 - для перехода по текущей ссылке: ')
-        if choice == '1':
-            browser.get(transit_link)
-            break
-
-    # Выбор варианта продолжения после перехода по ссылке
-    case_2 = input('Сделайте выбор:\n 0 - для завершения;\n 1 - листать параграфы на странице;\n 2 - перейти на одну из основных статей; \nВаш выбор: ')
-    if case_2 == '0':
+choice_a = input('Сделайте выбор:\n 0 - для завершения;\n 1 - листать параграфы на странице;\n 2 - перейти на одну из связанных страниц; \nВаш выбор: ')
+match choice_a:
+    case '0':
         print('Завершение программы')
         time.sleep(3)
-    elif case_2 == '1':
+    case '1':
         paragraphs = browser.find_elements(By.TAG_NAME, "p")
         print(f'Всего параграфов: {len(paragraphs)}')
         for paragraph in paragraphs:
@@ -88,23 +63,47 @@ elif case == '2':
             k = input('Нажмите Enter для вывода следующего параграфа или Q для завершения: ')
             if k.lower() == 'q':
                 break
-    elif case_2 == '2':
-        all_hatnotes = find_all_hatnotes(transit_link, browser)
-        if len(all_hatnotes) == 0:
-            print('На странице не найдено основных статей')
-            time.sleep(3)
-        else:
-            hatnote = random.choice(all_hatnotes)
-            # Для получения ссылки нужно найти на сайте тег "a"
-            hatnote_link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
-            browser.get(hatnote_link)
-            time.sleep(10)
-    else:
+    case '2':
+        all_links = find_all_links(enter_point, browser)
+        # Выбор подходящей ссылки для перехода
+        for title, link in all_links.items():
+            transit_link = link
+            print(f"Название ссылки: {title},\n ссылка: {link}\n")
+            link_select = input('Нажмите Enter, чтобы дальше листать ссылки, 1 - для перехода по текущей ссылке: ')
+            if link_select == '1':
+                browser.get(transit_link)
+                break
+        # Выбор варианта продолжения после перехода по ссылке
+        choice_b = input('Сделайте выбор:\n 0 - для завершения;\n 1 - листать параграфы на странице;\n 2 - перейти на одну из основных статей; \nВаш выбор: ')
+        match choice_b:
+            case '0':
+                print('Завершение программы')
+                time.sleep(3)
+            case '1':
+                paragraphs = browser.find_elements(By.TAG_NAME, "p")
+                print(f'Всего параграфов: {len(paragraphs)}')
+                for paragraph in paragraphs:
+                    print(paragraph.text, '\n')
+                    k = input('Нажмите Enter для вывода следующего параграфа или Q для завершения: ')
+                    if k.lower() == 'q':
+                        break
+            case '2':
+                all_hatnotes = find_all_hatnotes(transit_link, browser)
+                if len(all_hatnotes) == 0:
+                    print('На странице не найдено основных статей')
+                    time.sleep(3)
+                else:
+                    hatnote = random.choice(all_hatnotes)
+                    # Для получения ссылки нужно найти на сайте тег "a"
+                    hatnote_link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
+                    browser.get(hatnote_link)
+                    time.sleep(10)
+            case _:
+                print('Завершение программы')
+                time.sleep(3)
+    case _:
         print('Завершение программы')
         time.sleep(3)
-else:
-    print('Завершение программы')
-    time.sleep(3)
 
 browser.quit()
 
