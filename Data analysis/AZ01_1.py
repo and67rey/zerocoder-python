e21aa91c70f4739bc0aadb1f759d9780f1b901eb
Andrey <adulub@spinupper.com>
2025-02-03 09:07:01 -0500

    version 1.1

--- Data analysis/AZ01_1.py
+++ Data analysis/AZ01_1.py
@@ -4,8 +4,8 @@ data = pd.read_csv('Kepler Telescope Exoplanets.csv')
 df = pd.DataFrame(data)
 
 print(df.head())
-
+print()
 print(df.info())
-
+print()
 print(df.describe())
 
--- Data analysis/AZ01_2.py
+++ Data analysis/AZ01_2.py
@@ -4,13 +4,16 @@ data = pd.read_csv('dz.csv')
 df = pd.DataFrame(data)
 
 print(df)
+print()
 print(df.info())
+print()
 print(df.describe())
+print()
 
 # Удаление записи по Максиму, у которого Salary = NaN
 df.drop(7, inplace = True)
-
 print(df)
+print()
 
 group = df.groupby('City')['Salary'].mean()
 print(group)
