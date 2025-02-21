cd07a99633482e5f78bebe507417f179e3751c93
Andrey <adulub@spinupper.com>
2025-02-21 12:26:01 -0500

    VD04 v1.1

--- Web/VD04/date_time.py
+++ Web/VD04/date_time.py
@@ -4,11 +4,18 @@ from datetime import date, datetime
 app = Flask(__name__)
 
 @app.route("/")
-
 def date_and_time():
-    current_date = date.today()
-    current_time = datetime.now().time()
-    return f'Текущая дата: {current_date}, текущее время: {current_time}'
+    return '''
+    <html>
+        <head>
+            <meta http-equiv="refresh" content="1">
+        </head>
+        <body>
+            <p>Текущая дата: {}</p>
+            <p>Текущее время: {}</p>
+        </body>
+    </html>
+    '''.format(date.today(), datetime.now().time().replace(microsecond=0))
 
 if __name__ == "__main__":
     app.run()
